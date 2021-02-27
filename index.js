9b113ba606f4b2e3e241efe2f6115890e6600428
NatalyaDashkovska <dashkovsk@gmail.com>
2021-02-27 19:41:56 +0200

    fix

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
+const daysRef = document.querySelector(`[data-value="days"]`);
+const hoursRef = document.querySelector(`[data-value="hours"]`);
+const minutesRef = document.querySelector(`[data-value="mins"]`);
+const secondsRef = document.querySelector(`[data-value="secs"]`);
+
 class CountdownTimer {
   constructor(selector, targetDate) {
     this.selector = selector;
     this.targetDate = targetDate;
+    this.start();
+  }
+
+  start() {
+    setInterval(() => {
+      const currentTime = Date.now();
+
+      const time = this.selector.targetDate - currentTime;
+
+      this.updateTimer(time);
+    }, 1000);
+  }
+
+  updateTimer(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    daysRef.textContent = days;
+    hoursRef.textContent = hours;
+    minutesRef.textContent = mins;
+    secondsRef.textContent = secs;
+  }
+  pad(time) {
+    return String(time).padStart(2, '0');
   }
 }
 
@@ -9,33 +41,3 @@ const timerReverse = new CountdownTimer({
   selector: '#timer-1',
   targetDate: new Date('Jul 25, 2021'),
 });
-
-const start = () => {
-  setInterval(() => {
-    const currentTime = Date.now();
-    const time = timerReverse.selector.targetDate - currentTime;
-    updateTimer(time);
-  }, 1000);
-};
-
-const updateTimer = time => {
-  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-  const hours = pad(
-    Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-  );
-  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-
-  daysRef.textContent = days;
-  hoursRef.textContent = hours;
-  minutesRef.textContent = mins;
-  secondsRef.textContent = secs;
-};
-const pad = time => {
-  return String(time).padStart(2, '0');
-};
-const daysRef = document.querySelector(`[data-value="days"]`);
-const hoursRef = document.querySelector(`[data-value="hours"]`);
-const minutesRef = document.querySelector(`[data-value="mins"]`);
-const secondsRef = document.querySelector(`[data-value="secs"]`);
-start();
